20f6a6109c27471508a8009abbfe6cbab24a2273
Daniel Ramírez Sánchez <daniramisanch93@gmail.com>
2016-05-11 18:13:55 +0200

    Nueva actualización

--- PHP/BajaAyudante/BusquedaBajaAyudante.php
+++ PHP/BajaAyudante/BusquedaBajaAyudante.php
@@ -1,48 +1,39 @@
-<html>
-	<head>
-		<title>Buscador simple en PHP</title>
-	</head>
-	<body>
-		<form action="buscar.php" method="post">
-			Buscar: <input name="palabra">
-			<input type="submit" name="buscador" value="Buscar">
-		</form>
-		<?php
-			if ($_POST[‘buscador’])
-			{
-				// Tomamos el valor ingresado
-				$buscar = $_POST[‘palabra’];
+<?php
+//Si se ha pulsado el botón de buscar
+if (isset($_POST['search'])) {
+    //Recogemos las claves enviadas
+    $keywords = $_POST['keywords'];
 
-				// Si está vacío, lo informamos, sino realizamos la búsqueda
-				if(empty($buscar))
-				{
-					echo "No se ha ingresado una cadena a buscar";
-				}else{
-					// Conexión a la base de datos y seleccion de registros
-					$con=mysql_connect("localhost","usuario","password");
-					$sql = "SELECT * FROM noticias WHERE noticia like ‘%$buscar%’ ORDER BY id DESC";
-					mysql_select_db("base_de_datos", $con);
+    //Conectamos con la base de datos en la que vamos a buscar
+    $conexion = mysql_connect("localhost", "USUARIO", "PASSWORD");
+    mysql_select_db("NOMBRE_BASE_DE_DATOS", $conexion);
 
-					$result = mysql_query($sql, $con);
+    $query = "SELECT fecha, titulo, DATE_FORMAT(post_date, '%d-%m-%Y') as fecha
+                FROM TABLA
+                WHERE status = '1'
+                AND (contenido LIKE '%" . $keywords . "%'
+                OR titulo LIKE '%" . $keywords . "%')
+                ORDER BY fecha desc";
 
-					// Tomamos el total de los resultados
-					$total = mysql_num_rows($result);
+    $query_searched = mysql_query($query, $conexion);
 
-					// Imprimimos los resultados
-					if ($row = mysql_fetch_array($result)){
-						echo "Resultados para: <b>$buscar</b>";
-						do {
-					?>
-					<p><b><a href="noticia.php?id=<?=$row[‘id’];?>"><?=$row[‘titulo’];?></a></b></p>
-					<?
-					} while ($row = mysql_fetch_array($result));
-					echo "<p>Resultados: $total</p>";
-					} else {
-					// En caso de no encontrar resultados
-					echo "No se encontraron resultados para: <b>$buscar</b>";
-				}
-			}
-			}
-		?>
-	</body>
-</html>
+    $count_results = mysql_num_rows($query_searched);
+
+    //Si ha resultados
+    if ($count_results > 0) {
+
+        echo '<h2>Se han encontrado '.$count_results.' resultados.</h2>';
+
+        echo '<ul>';
+        while ($row_searched = mysql_fetch_array($query_searched)) {
+            //En este caso solo mostramos el titulo y fecha de la entrada
+            echo '<li><a href="#">'.$row_searched['titulo'].' ('.$row_searched['fecha'].')</a></li>';
+        }
+        echo '</ul>';
+    }
+    else {
+        //Si no hay registros encontrados
+        echo '<h2>No se encuentran resultados con los criterios de búsqueda.</h2>';
+    }
+}
+?>
