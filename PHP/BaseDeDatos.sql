62644ac791d833804113e8171eb84bf23a2faa9a
Daniel Ramírez Sánchez <daniramisanch93@gmail.com>
2016-05-12 11:32:03 +0200

    Cambios en la base de datos

--- PHP/BaseDeDatos.sql
+++ PHP/BaseDeDatos.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE mensajeria;
 USE mensajeria;
 
-CREATE TABLE contactos (
-	clavecontactos INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE tutores (
+	clavetutores INT AUTO_INCREMENT NOT NULL,
 	nombre VARCHAR(30) NOT NULL,
 	apellidos VARCHAR(50) NOT NULL,
-	telefono VARCHAR(15) NOT NULL,
+	alias VARCHAR(15) NOT NULL,
 	facultad VARCHAR(15) NOT NULL,
 	PRIMARY KEY (clavecontactos)
 ) ENGINE=InnoDB;
@@ -13,16 +13,25 @@ CREATE TABLE contactos (
 CREATE TABLE mensaje (
 	clavemensaje INT AUTO_INCREMENT NOT NULL,
 	texto VARCHAR(140) NOT NULL,
+	autor VARCHAR(20) NOT NULL,
 	PRIMARY KEY (clavemensaje)
 ) ENGINE=InnoDB;
 
-CREATE TABLE admin (
-	claveadmin INT AUTO_INCREMENT NOT NULL,
-	nombre VARCHAR(5) NOT NULL,
+CREATE TABLE usuarios (
+	claveusuarios INT AUTO_INCREMENT NOT NULL,
+	nombre VARCHAR(10) NOT NULL,
 	apellidos VARCHAR(40) NOT NULL,
 	contrasena VARCHAR(4) NOT NULL,
-	rango VARCHAR(10) NOT NULL,
 	PRIMARY KEY (claveadmin)
 ) ENGINE=InnoDB;
+
+CREATE TABLE envios (
+	claveenvios INT AUTO_INCREMENT NOT NULL,
+	fecha VARCHAR(10) NOT NULL,
+	mensaje
+	tutores
+	enviado VARCHAR(2) NOT NULL,
+	autor 
+)
 
 INSERT INTO admin VALUES(1, "admin","","uned","Administrador");
