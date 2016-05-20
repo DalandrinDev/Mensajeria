7b5041e8a89d586b05cb0060cbabefa85176224f
sirdan93 <daniramisanch93@gmail.com>
2016-05-20 11:43:19 +0200

    Delete VariablesSesion.php

--- VariablesSesion.php
+++
@@ -1,8 +0,0 @@
-<!--Este archivo va a contener todas las variables de sesión que se van a utilizar en el proyecto-->
-<!--De esta forma es más secillo cambiar las variables de sesion cuando quiera, y solo tendria que hacer una llamada al archivo-->
-<?php
-	session_start();
-	$sesionid = $_SESSION['id'];
-	$nombre = $_SESSION['nombre'];
-	$contrasena = $_SESSION['contrasena'];
-?>
