10a9cc1bf3d4d017e5d358dbf807146e0667df95
sirdan93 <daniramisanch93@gmail.com>
2016-05-20 11:41:02 +0200

    Delete EliminarTutor.php

--- EliminarTutor.php
+++
@@ -1,7 +0,0 @@
-<!--Ejecuta la consulta para eliminar al tutor deseado a través de su ID-->
-<?php
-	include '../conectar.php';
-	session_start();
-	$resultado=mysqli_query($link, "DELETE FROM tutores WHERE clavetutores='{$_SESSION['idtutores']}'");
-	header("location: ../home.php");
-?>
