054aba43e070596c2dd2f20d0116678d91d11fe3
Daniel Ramírez Sánchez <daniramisanch93@gmail.com>
2016-05-20 12:10:09 +0200

    Nueva actualizacion

--- PHP/GestionMensajes/Mensaje.php
+++ PHP/GestionMensajes/Mensaje.php
@@ -1,18 +1,45 @@
 <!--Es un formulario donde se almacenará el mensaje y se enviará pormetodo POST a EnviarMensaje.php-->
+<!DOCTYPE html>
 <html>
 	<head>
-		<body>
-			<form id="Mensaje" name="Mensaje" method="post" onsubmit="return validar();" action="EnviarMensaje.php" width="150" height="500">
-				<textarea class="form-control" rows="3" name="texto"></textarea>
-				<input type="submit" class="boton" value="Enviar">
-			</form>
-			<?php
-				include '../conectar.php';
-				session_start();
-				echo "El mensaje se enviará a: {$_SESSION['nombretutor']}";
-				echo "<br>";
-				echo "El mensaje estará firmado por: {$_SESSION['nombre']}";
-			?>
-		</body>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Mensajeria - Agregar Tutor</title>
+
+		<!-- Bootstrap Core CSS -->
+	    <link href="../../Recursos/css/bootstrap.min.css" rel="stylesheet">
+
+	    <!-- Custom CSS -->
+	    <link href="../../Recursos/css/grayscale.css" rel="stylesheet">
+
+	    <!-- Custom Fonts -->
+	    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+	    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 	</head>
+	<body>
+		<div class="container content-section text-center">
+			<form role="form" name="Mensaje" method="post" action="EnviarMensaje.php">
+				<div class="form-signin">
+					<h2 class="form-signin-heading">Escribe el mensaje</h2>
+					<?php
+						include '../conectar.php';
+						session_start();
+						echo "<p>El mensaje será firmado por: {$_SESSION['nombre']}</p>";
+					?>
+					<textarea class="form-control" rows="5" id="comment" name="texto"></textarea>
+					<button type="submit" class="btn btn-default">Enviar</button>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-8 col-lg-offset-2">
+			<input type="button" class="btn btn-danger" onclick="window.history.back();" value="Volver atras">
+		</div>
+	    <script src="../../Recursos/js/jquery.js"></script>
+	    <script src="../../Recursos/js/bootstrap.min.js"></script>
+		<script src="../../Recursos/js/jquery.easing.min.js"></script>
+	    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+		<script src="../../Recursos/js/grayscale.js"></script>
+	</body>
 </html>
