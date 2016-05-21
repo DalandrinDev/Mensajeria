29e84e7e01b5c3fd2947d19cb8754e21ca81d52c
Daniel Ramírez Sánchez <daniramisanch93@gmail.com>
2016-05-22 01:59:34 +0200

    Subida de Script

---
+++ PHP/script.py
@@ -0,0 +1,13 @@
+#!usr/bin/env
+import pexpect
+import time
+
+contacto = "Sergio"
+mensaje = "Este script funciona"
+telegram = pexpect.spawn("/var/www/tg/bin/telegram-cli -k /var/www/tg/tg-server.pub")
+telegram.expect('\r\n>', timeout=2)
+time.sleep(2)
+telegram.sendline("contact_list")
+telegram.sendline("msg "+contacto+""+mensaje)
+print ("Mensaje enviado a "+ contacto)
+telegram.sendline("quit")
